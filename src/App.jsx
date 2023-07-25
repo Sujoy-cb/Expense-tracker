ecfa0b37074a7945a50ee1b7bbb13eb95962d764
Sujoy chakraborty <sujoyc070@gmail.com>
2023-07-25 22:59:06 +0600

    expense 10%

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="main_part">
+      <div className="inner_part">
+        <h1 className="header">Expense Tracker</h1>
+        <div className="balance_part">
+        <div className="income">
+          <h3>Balance</h3>
+          <h6>$00.0</h6>
+        </div>
+        <div className="expense">
+          <h3>Expense</h3>
+          <h6>$00.0</h6>
+        </div>
+        </div>
+        <div className="input_part"></div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
